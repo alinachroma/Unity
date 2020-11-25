81f20ea8a3807c05cbbeb5e4d1361269ca6ad72b
chroma <40247900+alinachroma@users.noreply.github.com>
2020-11-25 18:54:35 +0100

    Abgabe3
    Abgabe3

--- Assets/Scripts/PlayGameObject.cs
+++ Assets/Scripts/PlayGameObject.cs
@@ -8,15 +8,19 @@ public class PlayGameObject : MonoBehaviour
     // Start is called before the first frame update
     public Transform myPrefab;
     public Transform Player;
+    public Transform ParentBear;
     public List<Transform> myObjects = new List<Transform>();   
 
     void Start()
     {
         for (int i = 0; i <= 10; i++)
         {
-            Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
 
-            myObjects.Add(Instantiate(myPrefab, pos, Quaternion.identity) as Transform);
+            Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+            Transform go = Instantiate(myPrefab, pos, Quaternion.identity);
+            //myObjects.Add();
+            go.SetParent(ParentBear);
+            myObjects.Add(go);
             myPrefab.transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time) * 360 * UnityEngine.Random.Range(-1.0f, 1.0f));
         }
 
@@ -26,14 +30,16 @@ public class PlayGameObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (myPrefab) {
 
             foreach (Transform t in myObjects)
             {
+                //t.SetParent(ParentBear);
                 t.transform.rotation = Quaternion.Euler(0, Mathf.Sin(5.0f * Time.deltaTime) * 360 * UnityEngine.Random.Range(-10.0f, 10.0f), 0);        
-
             }
         }
+        
     }
 }
 
